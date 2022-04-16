f74a0a9f47fb270f69f9be59b63aab65813b4af0
Bikash Katwal <hsakib2051@gmail.com>
2022-04-16 10:53:49 +0545

    Created Book Store application

---
+++ Bikash.BookStores/Bikash.BookStores/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bikash.BookStores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
